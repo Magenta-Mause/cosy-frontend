79ef707a916d050e1298fc47a8c4a8815e04ea8d
Janne <jabbekeipert@gmail.com>
2025-12-06 22:29:58 +0100

    minor: Styling adjustments - Format `LoginBanner` component and improve spacing for text and button alignment.

--- src/components/display/Login/LoginBanner/LoginBanner.tsx
+++ src/components/display/Login/LoginBanner/LoginBanner.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@components/ui/button";
-import { useTranslation } from "react-i18next";
+import {Button} from "@components/ui/button";
+import {useTranslation} from "react-i18next";
 
 const LoginBanner = (props: { setOpen: (open: boolean) => void }) => {
-  const { t } = useTranslation();
+  const {t} = useTranslation();
 
   return (
-    <div className="flex flex-row items-center gap-4 bg-primary-banner text-black text-3xl p-2 rounded-md border border-border">
-      {t("signIn.question")}
+    <div
+      className="flex flex-row items-center gap-8 bg-primary-banner text-black text-3xl p-2 rounded-md border border-border px-6">
+      <p>{t("signIn.question")}</p>
       <Button className="h-[80%]" onClick={() => props.setOpen(true)}>
         {t("signIn.signIn")}
       </Button>
