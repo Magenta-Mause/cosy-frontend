f9b04e1b465bff1500fc137195e88e9927758eb8
hyunjoonchoi <hyunjoonchoi04@gmail.com>
2025-12-14 13:15:39 +0100

    fix: added button varients

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import GameServerDisplay from "@components/display/GameServer/GameServerDisplay/GameServerDisplay.tsx";
 import { InviteRedemptionModal } from "@components/display/InviteRedemptionModal/InviteRedemptionModal.tsx";
-import LoginDisplay from "@components/display/Login/LoginDisplay/LoginDisplay";
+import LoginDisplay from "@components/display/Login/LoginDisplay/LoginDisplay.tsx";
 import { createFileRoute } from "@tanstack/react-router";
 import bgImage from "@/assets/ai-generated/main-page/background.png";
 import { useTypedSelector } from "@/stores/rootReducer.ts";
